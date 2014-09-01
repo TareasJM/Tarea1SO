d1372bb01dfd0175e25e495177edf07c549c8e77
Salinas <marco.salinas.12@sansano.usm.cl>
2014-09-01 16:43:51 -0400

    No crea dentro del dir

--- main.c
+++ main.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <dirent.h>
-
-int main (void)
-{
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+int main (int argc, char const *argv[])
+{ 
+  //Puntero al Directorio
   DIR *dp;
-  struct dirent *ep;     
-  dp = opendir ("./");
+  struct dirent *ep;
+  char *name_archive;
+  char *format_archive;
+  char *des;
+ 
+  dp = opendir (argv[1]);
 
   if (dp != NULL)
   {
     while (ep = readdir (dp))
-      puts (ep->d_name);
-
+    { 
+      //Obtiene el nombre completo del Archivo
+      name_archive = ep->d_name;
+      //Separa el Nombre del Archivo y el Formato 
+      strtok_r(name_archive,".",&format_archive);
+      if(format_archive != NULL)
+      { 
+        //Crea un directorio
+        des = strcat(des,format_archive);
+        mkdir(format_archive,0777);
+        printf("%s\n",format_archive);
+      }
+    }
     (void) closedir (dp);
   }
   else
