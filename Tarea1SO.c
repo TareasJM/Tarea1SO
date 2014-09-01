19c3af999cb62042a2790c3df33d8ebea9239b1b
n3ggro <n3ggro@gmail.com>
2014-09-01 19:29:35 -0400

    some libs removed

--- Tarea1SO.c
+++ Tarea1SO.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 
 off_t fsize(const char *filename);
 
